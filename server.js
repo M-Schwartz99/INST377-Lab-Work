3da60a3bba2dd505585160dca8e1cd3770b1690c
M-Schwartz99 <89875672+M-Schwartz99@users.noreply.github.com>
2021-10-06 22:24:44 -0400

    fixes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 import express from 'express';
-import labRoutes from './server/labRoutes.js';
-import apiRoutes from './server/routes/apiRoutes.js';
 import reload from 'livereload';
 import connectReload from 'connect-livereload';
 import dotenv from 'dotenv';
 import path from 'path';
+import apiRoutes from './server/routes/apiRoutes.js';
+import labRoutes from './server/labRoutes.js';
 
 dotenv.config();
 const __dirname = path.resolve();
@@ -34,15 +34,15 @@ app.use(express.json());
 app.use(express.static(staticFolder));
 // app.use('/template', labRoutes);
 app.use('/api', apiRoutes);
-
 async function bootServer() {
   try {
     app.listen(PORT, () => {
       // Turn these back on in later labs
       // const mysql = await db.sequelizeDB;
       // await mysql.sync();
+      process.env.CONTEXT = 'development';
       console.log(`Listening on: http//localhost:${PORT}`);
-      console.log(`environment:`, process.env.CONTEXT);
+      console.log('environment:', process.env.CONTEXT);
     });
   } catch (err) {
     console.error(err);
